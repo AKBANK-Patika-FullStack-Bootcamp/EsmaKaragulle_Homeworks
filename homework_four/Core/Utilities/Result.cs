6aa46db327439bc11db4aedd0028b1d6d7f6cd7d
esma6 <77541320+esma6@users.noreply.github.com>
2022-01-13 23:35:09 +0300

    homework4

---
+++ homework_four/Core/Utilities/Result.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+    }
+}
